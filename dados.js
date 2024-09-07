4a3f72c93cd57b418e9f60639d24a3c47c06b732
Neiwerton Cesar <neiwerton@outlook.com>
2024-09-06 21:17:37 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,24 @@
+let dados = [
+    {
+        titulo : "Bombom de coco",
+        descricao : "Uma Deliciosa Receita Rápida",
+        imagem: "BOMBOM-DE-COCO.jpg",
+        link : "https://www.sococo.com.br/receitas/bombom-de-coco/",
+        tags: "dicas doce coco ralado cocos"
+    },
+    {
+        titulo : "Bolo Três Leites",
+        descricao : "Ideal para um lanche da tarde",
+        imagem: "BoloTresLeites.jpg",
+        link : "https://www.sococo.com.br/receitas/bolo-tres-leites/",
+        tags: "dicas doce coco ralado cocada cremosa rápida cocos"
+    },
+    {
+        titulo : "Torta holandesa de coco",
+        descricao : "Delicioso para sobremesa",
+        imagem: "TORTAHOLANDESA.png",
+        link : "https://www.sococo.com.br/receitas/torta-holandesa-de-coco/",
+        tags: "dicas doce coco ralado cocada cremosa rápida cocos"
+    }
+];
+
